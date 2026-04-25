d166cef5bc65b17bf464327e80b7ed6855b429a1
YanuTriN <yanutrinugrahati@gmail.com>
2026-04-25 16:18:28 +0700

    Ubah tone warna jadi emerald

--- src/main.js
+++ src/main.js
@@ -22,18 +22,18 @@ const menuData = [
   // Fungsi untuk membuat kartu menu otomatis
   menuData.forEach(item => {
     menuGrid.innerHTML += `
-    <div class="flex flex-col h-full bg-white rounded-2xl overflow-hidden shadow-sm border border-slate-100">
+    <div class="flex flex-col h-full bg-emerald-100 rounded-2xl overflow-hidden shadow-sm border border-slate-100">
     <img src="${item.gambar}" style="height: 200px; width: 100%; object-fit: cover;" class="rounded-t-2xl">
     
     <div class="p-6 flex flex-col flex-grow">
-      <span class="text-xs font-bold text-pink-600 uppercase mb-1">${item.kategori}</span>
+      <span class="text-xs font-bold text-emerald-600 uppercase mb-1">${item.kategori}</span>
       <h3 class="text-lg font-bold text-slate-900">${item.nama}</h3>
       
       <p class="text-slate-500 text-sm mb-4 flex-grow">${item.deskripsi}</p>
       
       <div class="flex items-center justify-between pt-4 border-t border-slate-50">
         <span class="text-xl font-bold">Rp ${item.harga}</span>
-        <a href="#" class="bg-green-500 text-white px-4 py-2 rounded-lg text-sm">Pesan</a>
+        <a href="#" class="bg-green-500 text-black px-4 py-2 rounded-lg text-sm">Pesan</a>
       </div>
     </div>
   </div>
